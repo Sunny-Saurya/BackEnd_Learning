9ebab2276ca571032c90bb5d477ec00272e7b462
Sunny Kumar <sunnysaurya9955@gmail.com>
2024-09-10 18:34:28 +0530

    updated one

--- Middlewares/middleware_recap.js
+++ Middlewares/middleware_recap.js
@@ -31,3 +31,42 @@ app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
 
+// clearneer way to do this same is by making global function
+
+const express = require('express');
+const app = express();
+
+// Corrected middleware function with correct parameter order
+function isOldEnoughMiddleware(req, res, next){
+  const age = req.query.age;
+
+  if (age >= 14) {
+    next();  // Pass the request to the next handler if age is valid
+  } else {
+    res.send('You are not old enough');  // Send response if age is invalid
+  }
+}
+
+// Route handlers, no need to call the middleware again
+app.get('/ride1', isOldEnoughMiddleware, (req, res) => {
+  res.json({
+    msg: 'You have successfully booked ride 1'
+  });
+});
+
+app.get('/ride2', isOldEnoughMiddleware, (req, res) => {
+  res.json({
+    msg: 'You have successfully booked ride 2'
+  });
+});
+
+app.get('/ride3', isOldEnoughMiddleware, (req, res) => {
+  res.json({
+    msg: 'You have successfully booked ride 3'
+  });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
